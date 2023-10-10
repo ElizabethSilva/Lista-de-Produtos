7dfeacb27a7fedd37ced61487835f4a15786b6ab
Elizabeth Tavares <elizabeth.silva.cristina@gmail.com>
2023-10-10 02:09:15 -0300

    Adiciona novos recursos ao projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ProductsData from './products.json';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [products, setProducts] = useState(ProductsData.produtos);
+
+  const filteredProducts = products.filter(product =>
+    product.nome.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const classification = (value) => {
+    if (value === 'menorPreco') {
+      // Ordenar produtos por menor preço
+      const sortedProducts = products.slice().sort((a, b) => a.preco - b.preco);
+      setProducts(sortedProducts);
+    } else if (value === 'maiorPreco') {
+      // Ordenar produtos por maior preço
+      const sortedProducts = products.slice().sort((a, b) => b.preco - a.preco);
+      setProducts(sortedProducts);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top'>
+        <input className="search-top" type="search" placeholder="Buscar produto" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <div className='form'>
+          <label>Classificar por:</label>
+          <select className='rank' onChange={(e) => classification(e.target.value)}>
+            <option value="" disabled selected>Escolha uma opção</option>
+            <option value="menorPreco">Menor Preço</option>
+            <option value="maiorPreco">Maior Preço</option>
+          </select>
+        </div>
+      </div>
+
+      <div className='box'>
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div key={product.nome} className="product-item">
+              <div className="image-container">
+                <img src={product.img} alt={product.nome} />
+              </div>
+              <strong>{product.nome}</strong> <br /><br />
+              <strong>{product.preco}</strong> <br />
+              <div className='button'>
+                <a href={product.link} target="_blank" rel="noopener noreferrer">
+                  <button className="buy">Comprar</button>
+                </a>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div><strong>Nenhum produto encontrado.</strong></div>
+        )}
+      </div>
     </div>
   );
 }
